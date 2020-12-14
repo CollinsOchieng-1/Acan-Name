746b4faf573a0ec7467c4c6a71629888a87f3d7e
Moringa School <collinsovhieng-1>
2020-12-14 08:16:30 +0300

    addition

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var (century, year, month, dayOfTheMonth, dayOfTheWeek, day);
+var century, year, month, dayOfTheMonth, dayOfTheWeek, day;
 //Get input
 function getInput(){
   century = parseInt(document.getElementById("century").value);
   year = parseInt(document.getElementById("year").value);
   month = parseInt(document.getElementById("month").value);
-  dayOfTheMonth = parseInt(document.getElementById("days").value);
+  dayOfTheMonth = parseInt(document.getElementById("days").value
