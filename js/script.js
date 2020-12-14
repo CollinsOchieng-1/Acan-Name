e79bd6dcc2f792b2d265062627f9b36ec6f2a967
Moringa School <collinsovhieng-1>
2020-12-14 15:00:57 +0300

    all

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-var century, year, month, dayOfTheMonth, dayOfTheWeek, day;
-//Get input
-function getInput(){
-  century = parseInt(document.getElementById("century").value);
-  year = parseInt(document.getElementById("year").value);
-  month = parseInt(document.getElementById("month").value);
-  dayOfTheMonth = parseInt(document.getElementById("days").value
+var=function
